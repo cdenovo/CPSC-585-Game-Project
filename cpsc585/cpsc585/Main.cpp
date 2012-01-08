51cdb16bd4716725c3718f4db013e323a2c4f81b
Charles D <cdebavel@ucalgary.ca>
2012-01-07 17:36:20 -0700

    Adding empty VS solution (should be configured with the correct DirextX includes)

---
+++ cpsc585/cpsc585/Main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("This is a test.");
+	return;
+}
---
+++ cpsc585/cpsc585/Main.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+
+class Main
+{
+public:
+	Main(void);
+	~Main(void);
+};
+
