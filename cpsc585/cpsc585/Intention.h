aac9bab22113e33ef682bcc503c54cc3d1ef5b6e
Joshua Foley <therpgmaker@hotmail.com>
2012-02-05 20:33:11 -0700

    Added serialization of intention class to be sent over network

--- cpsc585/cpsc585/Intention.h
+++ cpsc585/cpsc585/Intention.h
@@ -1,10 +1,21 @@
 #pragma once
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
 class Intention
 {
 public:
 	Intention(void);
 	~Intention(void);
 	void reset();
+
+	void serialize(char bitstr[]);
+	void unserialize(char bitstr[]);
+
+	string toStr();
 
 	// Buttons Pressed State
 	bool xPressed;
