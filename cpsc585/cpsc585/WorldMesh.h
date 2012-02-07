b035eb75d332d7ac8e481d78379179c913cec4f3
Charles D <cdebavel@ucalgary.ca>
2012-02-06 18:18:17 -0700

    For some reason these 3 were not included in the last push.

---
+++ cpsc585/cpsc585/WorldMesh.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Mesh.h"
+class WorldMesh :
+	public Mesh
+{
+public:
+	static WorldMesh* getInstance(IDirect3DDevice9* device);
+	~WorldMesh(void);
+	void render(IDirect3DDevice9* device);
+protected:
+	WorldMesh(IDirect3DDevice9* device);
+
+private:
+	void initialize(IDirect3DDevice9* device);
+
+
+public:
+
+private:
+	static WorldMesh* mesh;
+};
