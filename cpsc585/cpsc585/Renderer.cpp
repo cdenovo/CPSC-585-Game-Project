a2045ee7915b354339a5b0e1fa3eb151e9173d1c
Charles D <cdebavel@ucalgary.ca>
2012-03-27 21:33:23 -0600

    Small tweak that should slightly boost performance

--- cpsc585/cpsc585/Renderer.cpp
+++ cpsc585/cpsc585/Renderer.cpp
@@ -224,7 +224,8 @@ void Renderer::render()
 	camera->getViewMatrix(viewMatrix);
 
 
-	device->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(10, 10, 80), 1.0, 0);
+	device->Clear(0, NULL, D3DCLEAR_ZBUFFER, NULL, 1.0, 0);
+	
 
 	device->SetTransform(D3DTS_PROJECTION, &projectionMatrix);
 	device->SetTransform(D3DTS_VIEW, &viewMatrix);
